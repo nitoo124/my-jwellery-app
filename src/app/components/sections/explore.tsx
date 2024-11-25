27c9b8ebe26c4e09a064313f0d9f38fef9f094e0
Nitoo kumari <160069703+nitoo124@users.noreply.github.com>
2024-11-24 16:27:23 -0800

    updated

--- src/app/components/sections/explore.tsx
+++ src/app/components/sections/explore.tsx
@@ -20,7 +20,7 @@ function Explore() {
                 <div className=" bg-black text-zinc-300  shadow-dark rounded-xl p-4 cursor-pointer scale-95 hover:scale-100 duratiom-500 text-center" data-aos = 'fade-left'  data-aos-delay='200'>
                     <h2 className='mt-3 text-2xl font-bold text-shadow-lg'>Stylish Necklace</h2>
                     <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, ratione.</p>
-                    <Image className='mt-4 mx-auto ' src={'/Products/collection1.png'} alt='stylish bracelate' width={300} height={300} loading='lazy' />
+                    <Image className='mt-4 mx-auto ' src={'/collection/collection1.png'} alt='stylish bracelate' width={300} height={300} loading='lazy' />
                     <div className='my-3 text-center'>
                         <Button text='Explore Now' bg='bg-zinc-300' textcolor='text-black ' />
                     </div>
@@ -32,7 +32,7 @@ function Explore() {
                     <h2 className='mt-3 text-2xl font-bold text-shadow-lg'>Stylish Necklace </h2>
                     <p className='text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, fugit. </p>
 
-                    <Image className='mt-4 mx-auto' src={'/Products/collection2.png'} alt='stylish bracelate' width={300} height={300} loading='lazy' />
+                    <Image className='mt-4 mx-auto' src={'/collection/collection2.png'} alt='stylish bracelate' width={300} height={300} loading='lazy' />
                     <div className='my-3 text-center'>
                         <Button text='Explore Now' bg='bg-zinc-300' textcolor='text-black' />
                     </div>
@@ -43,7 +43,7 @@ function Explore() {
                 <div className=" bg-black text-zinc-300  shadow-dark  rounded-xl p-4 cursor-pointer  scale-95 hover:scale-100 duratiom-500 text-center" data-aos = 'fade-left'  data-aos-delay='600'>
                     <h2 className='mt-3 text-2xl font-bold text-shadow-lg'>Stylish Bangles</h2>
                     <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, quidem.</p>
-                    <Image className='mt-4 mx-auto' src={'/Products/collection3.png'} alt='stylish bracelate' width={300} height={300} loading='lazy' />
+                    <Image className='mt-4 mx-auto' src={'/collection/collection3.png'} alt='stylish bracelate' width={300} height={300} loading='lazy' />
                     <div className='my-3 text-center'>
                         <Button text='Explore Now' bg='bg-zinc-300' textcolor='text-black ' />
                     </div>
